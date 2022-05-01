574406d6b6beae16a4b2e0acad6aa973c8e13da8
bariknishan <barik6286@gmail.com>
2022-05-01 14:27:00 +0600

    query added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const port = process.env. PORT || 5000 ;
 const app =express() ;
 
@@ -21,14 +21,46 @@ app.use(express.json())
  ;
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqpm0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- client.connect(err => {
-   const collection = client.db("test").collection("devices");
-   console.log('computer server is connected')
-   // perform actions on the collection object
-   client.close();
- });
- 
 
+
+ async function run (){
+ try{
+
+    await client.connect();
+    const itemsCollection = client.db('ComputerWarehouse').collection('items');
+
+ //for all user
+
+   app.get('/items', async(req,res)=>{
+    const query={}
+    const cursor = itemsCollection.find(query)
+    const itemsBundle= await cursor.toArray();
+    res.send(itemsBundle)
+   });
+
+
+ //// for single user
+
+  app.get('/items/:id', async(req,res)=>{
+
+    const id = req.params.id ;
+    const query={_id:ObjectId(id)};
+    const items= await itemsCollection.findOne(query);
+    res.send(items)
+  })
+
+
+
+ }
+
+  finally{
+
+}
+
+
+ }
+
+ run().catch(console.dir);
 
 
 
