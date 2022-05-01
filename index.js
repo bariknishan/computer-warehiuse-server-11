f297d06aa062b231ddee3ed93b3679509058489a
bariknishan <barik6286@gmail.com>
2022-05-01 17:55:25 +0600

    database connected

--- index.js
+++ index.js
@@ -47,8 +47,16 @@ app.use(express.json())
     const query={_id:ObjectId(id)};
     const items= await itemsCollection.findOne(query);
     res.send(items)
-  })
+  });
 
+// post  area 
+
+ app.post('/items' , async(req,res)=>{
+
+    const newItems= req.body ;
+    const result = await itemsCollection.insertOne(newItems)
+    res.send(result)
+ })
 
 
  }
