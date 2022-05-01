7ebe015e2b23eb277395c4b497d38782cf3b0f0c
bariknishan <barik6286@gmail.com>
2022-05-01 12:27:48 +0600

    env added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env. PORT || 5000 ;
 const app =express() ;
 
@@ -16,6 +18,16 @@ app.use(express.json())
  ////// connection///////////
 
 
+ ;
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqpm0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ client.connect(err => {
+   const collection = client.db("test").collection("devices");
+   console.log('computer server is connected')
+   // perform actions on the collection object
+   client.close();
+ });
+ 
 
 
 
